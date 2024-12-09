200312bd203cf53f49b2a53e82fbe037fd41ffb9
allisonRabia <09snippet-frazzle@icloud.com>
2024-12-09 13:12:49 -0600

    hellolist

--- hello-spring/hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring/hello-spring/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -1,8 +1,13 @@
 
 package org.launchcode.hello_spring.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
@@ -23,23 +28,31 @@ public class HelloController {
 
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     //handle requests of the form http://localhost:8080/hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
     }
-
+    @GetMapping("hello-names")
+    public String helloNames() {}
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("LaunchCode");
+        names.add("LaunchCode");
+        return "hello-list";
 }
 //
 //package org.launchcode.hello_spring.controllers;
@@ -47,7 +60,7 @@ public class HelloController {
 //import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.*;
 //
-///**
+//**
 // * Created by Chris Bay
 // */
 //@Controller
